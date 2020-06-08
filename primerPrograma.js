1ce80968c9519c4c3792d4cb9b1fa1da2bea1b77
Ramon Cervera <rcerver4@xtec.cat>
2020-06-08 18:45:32 +0200

    primer commit

--- primerPrograma.js
+++ primerPrograma.js
@@ -2,5 +2,6 @@ console.log('Hola!');
 const moduls = ['programació web','bases de dades','Sistemes operatius'];
 let a = 3 + 4 ;
 console.log (moduls);
+console.log(moduls[0]);
 console.log(a);
 console.log('adeu');
